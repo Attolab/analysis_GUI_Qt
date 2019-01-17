2129889c030dc0275613555d35eefd92d3e7091f
dbrestea <david.bresteau@cea.fr>
2019-01-17 18:18:59 +0100

    Review comments and return the real field instead of the intensity

--- Rabbit/pulse_in_time_win.py
+++ Rabbit/pulse_in_time_win.py
@@ -10,7 +10,7 @@ import numpy as np
 import glob_var as cts
 
 class PulseInTimeWin(QDialog):
-    ''' created when clicking on the "[plot] SB vs delay" button'''
+    ''' created when clicking on the "Pulse in Time" button'''
     def __init__(self, parent): # parent=RabbitWin
         super(PulseInTimeWin, self).__init__(parent)
         self.setGeometry(300, 300, 1000, 500)
@@ -45,8 +45,6 @@ class PulseInTimeWin(QDialog):
             self.ax.set_xlabel("Time", fontsize=10)
             self.ax.set_ylabel("Intensity", fontsize=10)
 
-            # DEBUG
-            # self.ax.plot(cts.energy_vect, cts.rabbit_mat[0, :])
             self.ax.plot(time_axis, self.temporal_profile_fn(time_axis))
 
             self.fc.draw()
@@ -56,12 +54,10 @@ class PulseInTimeWin(QDialog):
 
     # This fonction calculates the intensity of the pulse. It uses cts.peak_phase
     # which contains the spectral dephasings between two consecutive harmonics
-    # We suppose that is has the form cts.peak_phase = [phase_SB_14, phase_SB_16, …]
+    # We suppose that it has the form cts.peak_phase = [phase_SB_14, phase_SB_16, …]
     def temporal_profile_fn(self, time):
 
         harmonics_spectral_phases = []
-        # DEBUG
-        # DEBUG_phases = [1, 2, 3, 4, 8]
 
         # In this loop we calculate the harmonics spectral phases.
         # The first harmonic phase is zero by convention.
@@ -70,21 +66,12 @@ class PulseInTimeWin(QDialog):
             spectral_phase = 0.0
 
             for j in range(i):
-                # DEBUG
-                # spectral_phase -= DEBUG_phases[j]
                 spectral_phase -= cts.peak_phase[j]
 
             harmonics_spectral_phases.append(spectral_phase)
 
-        # DEBUG
-        # print(DEBUG_phases)
-        # print(harmonics_spectral_phases)
-        # print(cts.energy_vect)
-
         # Construct the amplitudes of the harmonics
         h_nu = cts.HEV * cts.cur_nu
-        # harmonics_energies = []
-        # harmonics_energy_arguments = []
         harmonics_amplitudes = []
 
         for i in range(cts.bandsnb + 1):
@@ -94,17 +81,13 @@ class PulseInTimeWin(QDialog):
             # We take arbitrarily the amplitudes of the harmonics at delay zero
             harmonics_amplitudes.append(cts.rabbit_mat[0, harmonic_energy_argument])
 
-        # print(harmonics_energies)
         print(harmonics_amplitudes)
 
         # fields_sum is the sum of the fields of each harmonic
         fields_sum = 0.0
 
-        # fields_sum = harmonics_amplitudes[0] * np.exp(
-        #     - 2 * 1j * np.pi * (cts.first_harm) * cts.cur_nu * time + harmonics_spectral_phases[0])
-
         for i in range(cts.bandsnb + 1):
             fields_sum += harmonics_amplitudes[i] * np.exp(- 2 * 1j * np.pi * (cts.first_harm + 2*i) * cts.cur_nu * time + harmonics_spectral_phases[i])
 
-        # Return the intensity of the calculated field
-        return np.abs(fields_sum)**2
+        # Return the calculated field
+        return np.real(fields_sum)
