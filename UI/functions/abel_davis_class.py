20f038adeae93ac5a967329fedaa5e8b9481d064
Ashwolaa <schouderconstant@outlook.com>
2021-12-17 18:29:10 +0100

    Faster matrix creation (can still be improved)

--- UI/functions/abel_davis_class.py
+++ UI/functions/abel_davis_class.py
@@ -1,15 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from abel.tools import polar
-from numpy.ma.core import not_equal
 from scipy.special import eval_legendre, hyp2f1
 from scipy.interpolate import interp1d
 from math import factorial
-import os.path
 import time
-
+import h5py
+import os
 class Abel_object():
-    def __init__(self, data, center_x, center_y, d_alpha_deg, dr, N, Rmax, parent=None):
+    def __init__(self, data, center_rows, center_cols, d_alpha_deg, dr, N, Rmax, parent=None):
         """
             Creates an object that can Abel invert an image using the DAVIS
             (Direct Algorithm for Velocity-map Imaging) technique described in
@@ -19,11 +18,10 @@ class Abel_object():
             ----------
             data : 2D np.array
                 The data to invert
-            center_x : int
-                x coordinate of the center of the image, corresponds to vertical axis
-                if the data is transposed
-            center_y : int
-                y coordinate of the center of the image
+            center_rows : int
+                Row coordinate of the center of the image, corresponds to the symmetry axis (polarization/propagation)
+            center_cols : int
+                Col coordinate of the center of the image, correspond to the other axis
             d_alpha_deg : float
                 Angular coordinate spacing (in degrees)
             dr : int
@@ -56,12 +54,13 @@ class Abel_object():
         self.alpha_vector = np.linspace(2 * np.pi / self.N_alpha, 2 * np.pi, self.N_alpha)
 
         self.data_polar, r_grid, theta_grid = polar.reproject_image_into_polar(
-            data, origin=(center_x, center_y), dr=dr, dt=self.d_alpha, Jacobian=True)
+            data, origin=(center_rows, center_cols), dr=dr, dt=self.d_alpha, Jacobian=True)
         self.alpha_vector = theta_grid[0,:]
 
         self.M_inv = {}
         self.Mnk = {}
         self.F = {}
+        self.beta ={}
 
     def set_data(self, data):
         if data.shape != (self.Ny, self.Nx):
@@ -72,7 +71,6 @@ class Abel_object():
 
     def show(self, data):
         plt.figure()
-        # g = plt.pcolormesh(data.T)
         g = plt.imshow(data, cmap='jet', origin='lower')
         plt.colorbar(g)
         plt.show()
@@ -133,26 +131,20 @@ class Abel_object():
             ''' This function calculates the upper triangular transformation matrices Mn,n-2k using the analytical formulas
             from Table I. I checked that it's equal to M_eqn13(N_R,n,k). However it's faster than M_eq13 so this is the function
             actually used '''
-            d_alpha = self.d_alpha
-            dr_over2 = self.dr/2
             M = np.zeros((N_R,N_R))
-            coeff = dr*d_alpha
-
-
-            for ip in range (0, N_R):
-                R_ip = self.R_vector[ip]
-                R_minus = self.R_vector - dr_over2 # Ri v = Ri - DeltaR/2
-                # if ip == i:
-                #     R_plus = self.R_vector[i]
-                # else:
-                R_plus = self.R_vector + dr_over2 # Ri ^ = Ri + DeltaR/2
-                
+            dr_over2 = self.dr/2
+            coeff = self.dr*self.d_alpha
+            
+            for i in range(0,N_R):
+                R_ip = self.R_vector[i:N_R]
+                R_minus = R_ip[0] - dr_over2 # Ri v = Ri - DeltaR/2
+                R_plus = R_ip[0] + dr_over2 # Ri v = Ri - DeltaR/2
                 R_plus_frac = R_plus/R_ip  # Ri ^ / Ri
-                R_plus_frac[0] = self.R_vector[i]/R_ip  # Ri ^ / Ri
-
+                R_plus_frac[0] = 1  # Force first value to be 1 to keep the square root well defined
                 R_minus_frac = R_minus/R_ip # Ri v  / Ri 
                 sqrtR_plus = np.sqrt(1-(R_plus_frac)**2) # (1 - (Ri ^  / Ri)^2)^1/2
                 sqrtR_minus = np.sqrt(1-(R_minus_frac)**2) # (1 - (Ri v  / Ri)^2)^1/2
+
                 if np.mod(n,2) != 0:
                     asin_Rdiff =  np.arcsin(R_plus_frac) - np.arcsin(R_minus_frac) # asin(Ri ^ / Ri) - asin(Ri v / Ri)
                 if(n==0 and k==0): # M00
@@ -219,44 +211,13 @@ class Abel_object():
                             (sqrtR_plus**3*(-536 - 1329*R_plus_frac**2 + 2670*R_plus_frac**4)
                             -sqrtR_minus**3*(-536 - 1329*R_minus_frac**2 + 2670*R_minus_frac**4))
                 else: #Take the numerical solution otherwise
-                    M_temp = np.array([self.C(n,k,l)*self.dr*self.d_alpha* \
-                                        self.Gammma(n,k,l,i,ip) for l in range(0,max(k-1,0)+1)]).sum()                                
+                    for ip in range(i,N_R):
+                        M_temp = np.array([self.C(n,k,l)*self.dr*self.d_alpha* \
+                                            self.Gammma(n,k,l,i,ip) for l in range(0,max(k-1,0)+1)]).sum()                                
                 M[i,i:N_R] = M_temp
                 
-            return M        
+            return M
 
-    def precalculate2(self):
-        N = self.N
-        filename = 'dr_' + (str(self.dr) + '_da_' + '{:.2f}'.format(self.d_alpha_deg) + '_N_' + \
-                            str(self.N) + '_center_x_' + \
-                            str(self.center_x) + '_y_' + str(self.center_y) + \
-                            '_Rmax_' + str(self.Rmax)).replace('.','p') + '.npy'
-        print(filename)
-        if os.path.isfile('M_inv_'+filename):
-            print('matrices already precalculated, loading them...')
-            self.M_inv = np.load('M_inv_'+filename, allow_pickle='TRUE').item()
-            self.Mnk = np.load('Mnk_'+filename, allow_pickle='TRUE').item()
-        else:
-            print('precalculating matrices...')
-            for k in range(0, 2 * N + 1):
-                self.M_inv[k] = np.linalg.inv(self.M(self.N_R, k, 0))  # inverse of M_kk
-                try:
-                    self.parent.progress_precalc.setValue(int((k + 1) / (2 * N + 1) * 50))
-                    self.parent.progress_precalc.repaint()
-                except AttributeError:
-                    print(int((k + 1) / (2 * N + 1) * 50), " %")
-            for k in range(2 * N, -1, -1):  # reversed loop from 2N to 0 included
-                N_threshold = N - ((k+1) // 2) #Threshold for entering the sum in equation 19 if smaller than N                                        
-                for i in range(N_threshold,0,-1):  #Going backwards since range function in Python is a real pain  
-                    self.Mnk[(k + 2 * i, i)] = self.M(self.N_R, k + 2 * i, i)
-                try:
-                    self.parent.progress_precalc.setValue(int((2 * N + 1 - k) / (2 * N + 1) * 50 + 50))
-                    self.parent.progress_precalc.repaint()
-                except AttributeError:
-                    print(int((2 * N + 1 - k) / (2 * N + 1) * 50 + 50), " %")
-            print('precalculation done, saving matrices as ', filename)
-            np.save('M_inv_' + filename, self.M_inv)
-            np.save('Mnk_' + filename, self.Mnk)
     def precalculate(self):
         N = self.N
         filename = 'dr_' + (str(self.dr) + '_da_' + '{:.2f}'.format(self.d_alpha_deg) + '_N_' + \
@@ -270,75 +231,94 @@ class Abel_object():
             self.Mnk = np.load('Mnk_'+filename, allow_pickle='TRUE').item()
         else:
             print('precalculating matrices...')
-            for k in range(0, 2 * N + 1):
+            for k in range(2 * self.N, -1, -1):  # reversed loop from 2N to 0 included
                 self.M_inv[k] = np.linalg.inv(self.M(self.N_R, k, 0))  # inverse of M_kk
+                N_threshold = self.N - ((k+1) // 2) #Threshold for entering the sum in equation 19 if smaller than N                                        
+                for i in range(N_threshold,0,-1):  #Going backwards since range function in Python is a real pain  
+                    self.Mnk[(k + 2 * i, i)] = self.M(self.N_R, k + 2 * i, i)
                 try:
-                    self.parent.progress_precalc.setValue(int((k + 1) / (2 * N + 1) * 50))
+                    self.parent.progress_precalc.setValue(int((2*N-k + 1) / (2 * self.N + 1) * 100))
                     self.parent.progress_precalc.repaint()
                 except AttributeError:
-                    print(int((k + 1) / (2 * N + 1) * 50), " %")
+                    print(int((2*self.N - k + 1) / (2 * self.N + 1) * 100), " %")                    
 
-            for k in range(2 * N, -1, -1):  # reversed loop from 2N to 0 included
-                if k % 2 == 0:  # even k
-                    if k == 2 * N:  # no sum over i
-                        pass
-                    elif k == 2 * N - 2:  # only one term in the sum
-                        i = 1
-                        self.Mnk[(k + 2 * i, i)] = self.M(self.N_R, k + 2 * i, i)
-                    else:
-                        for i in range(1, N - k // 2 + 1):
-                            self.Mnk[(k + 2 * i, i)] = self.M(self.N_R, k + 2 * i, i)
-                elif k % 2 == 1:  # odd k
-                    for i in range(1, N - (int(k / 2) + 1) + 1):
-                        self.Mnk[(k + 2 * i, i)] = self.M(self.N_R, k + 2 * i, i)
-                try:
-                    self.parent.progress_precalc.setValue(int((2 * N + 1 - k) / (2 * N + 1) * 50 + 50))
-                    self.parent.progress_precalc.repaint()
-                except AttributeError:
-                    print(int((2 * N + 1 - k) / (2 * N + 1) * 50 + 50), " %")
             print('precalculation done, saving matrices as ', filename)
             np.save('M_inv_' + filename, self.M_inv)
             np.save('Mnk_' + filename, self.Mnk)
 
-    def invert(self): 
-        N = self.N
-        # Application of eqn. 16
+    def invert(self):         
         # Note (Constant): I removed the definition from Dominique who was first storing the delta_k
         # and then using them to calculate the matrice element.
         # Since we go from high k to low k, we can only load and work with
         # only one at a time avoiding unecessary loops.
-        for k in range(2 * N, -1, -1):
-            Ri = np.arange(0,self.N_R)
+        Th_dist = self.data_polar[np.arange(0,self.N_R), :] #Theta distribution for all radius
+        sTh = np.abs(np.sin(self.alpha_vector)) # Absolute value of sin(theta)
+        cTh = np.cos(self.alpha_vector) # cos(theta)
+        for k in range(2 * self.N, -1, -1):
+            # Application of eqn. 16
             delta = (2 * k + 1) / 2 * \
-                    np.trapz(np.abs(np.sin(self.alpha_vector))*\
-                    eval_legendre(k, np.cos(self.alpha_vector))*\
-                    self.data_polar[Ri, :],dx=self.d_alpha) 
+                    np.trapz(sTh*eval_legendre(k, cTh)*Th_dist,dx=self.d_alpha)                     
                         # new note from Constant (2021/10/25):
                         # the coefficient has been removed such that the definition is consistent with the article
                         # old note from Dominique:
                         # I added the 0.5 because we integrate between 0 and 2pi
                         # instead of between 0 and pi
-        # Application of eqn. 19 
-        # for k in range(2 * N, -1 ,-1): # reversed loop from 2N to 0 included
-            N_threshold = N - ((k+1) // 2) #Threshold for entering the sum in equation 19 if smaller than N        
-            m2 = delta - np.array([np.dot(self.Mnk[(k + 2 * i, i)], self.F[k + 2 * i]) \
-                for i in range(N_threshold,0,-1)]).sum(axis=0)  #Going backwards since range function in Python is a real pain                             
-            self.F[k] = np.dot(self.M_inv[k], m2)     
+            # Application of eqn. 19 
+            N_threshold = self.N - ((k+1) // 2) #Threshold for entering the sum in equation 19 if smaller than N 
+            # Right side of eqn.19
+            m2 = delta                
+            if N_threshold:
+                m2 -= np.array([np.dot(self.Mnk[(k + 2 * i, i)], self.F[k + 2 * i]) \
+                for i in range(N_threshold,0,-1)]).sum(axis=0)  #Going backwards since range function in Python is a real pain                                  
+            self.F[k] = np.dot(self.M_inv[k],m2)
+
+    def calculate_beta(self):
+        for k in range(2 * self.N, -1, -1):
+            self.beta[k] = self.F[k]/self.F[0]
+
+    def make_inverse_image(self,L,Nmax = 6,even = 0):       
+        if np.mod(L,2):
+            L = L + 1     
+        X_vect = np.arange(-L/2,L/2)
+        Y_vect = np.arange(-L/2,L/2)
+        S = np.zeros([L**2]) #Image
+        R = np.sqrt(X_vect**2+Y_vect[:,np.newaxis]**2).flatten()
+        cosTh = np.cos(np.arctan2(X_vect,Y_vect[:,np.newaxis])).flatten()
+        mask = np.logical_and(R>0, R<self.Rmax)
+        cosTh = cosTh[mask]
+        R = R[mask]
+        beta_range = np.arange(2*Nmax)
+        if even:
+            beta_range = beta_range[np.array(np.mod(beta_range,2),dtype = bool) == False] #Thats way too long for something so simple...
+        for i in beta_range:
+            h = interp1d(self.R_vector, abel_obj.F[i]) 
+            S[mask] += h(R)*eval_legendre(i,cosTh) 
+        return S.reshape([L,L])
 
 if __name__ == '__main__':
 
 
     path = 'Q:\LIDyL\Atto\ATTOLAB\SE1\Data_Experiments\SE1_2021\\2021-02-15\\'
-    filename='Ar-He_PE_Vrep1p0_Vext_0p794_MCP700+300_PMCP2p6em5_7p1em6_avg100_WITHfilter_WITHdrilled_WITHSB_2p3W_better.npy'
-    data = np.load(path+filename)
+    filename='Ar-He_PE_Vrep1p0_Vext_0p794_MCP700+300_PMCP2p6em5_7p1em6_avg100_WITHfilter_WITHdrilled_WITHSB_2p3W_better.npy'    
+    # data = np.load(path+filename)
+
+    filename = 'D:\\Work\\Saclay\\Git\\analysis_GUI_Qt\\Dataset_20210423_003.h5'   
+    scan = '000'  # vérifier dans hdfview
+    path2 = ''.join(['Scan',scan,'/Detector000/Data2D/Ch000/Data'])
+    with h5py.File(filename, 'r') as file:                          
+        raw_datas = file['Raw_datas']   
+        data = np.asarray(raw_datas[path2][0])
+
+
+
 
     ############# INPUT PARAMETERS ##########################################
-    transpose = True
+    transpose = False
     remove_bkg = False
     # Image orientation:  “television” convention, where IM[0, 0] refers to the upper left corner of the image. 
     # This definition might not be consistent with the older definition of pyabel
-    center_x = 992  # Horizontal axis (COL)
-    center_y = 1018   # Vertical axis (ROW)
+    center_x = 1012  # Horizontal axis (COL)
+    center_y = 1037   # Vertical axis (ROW)
     #Check ouput of abel.tools.center functions to test:
     #  abel.tools.center.center_image(data)
 
@@ -346,22 +326,24 @@ if __name__ == '__main__':
     dr = 1  # increment in radius r
     N = 3  # number of photons. Determines the number of legendre polynomials to use (which is equal to 2N+1)
     # ex: if N=1 then P0, P1 and P2 are used. If N=2 then P0 to P4 are used.
-    Rmax = 1000  # maximum radius up to which the image is inverted
+    Rmax = 300  # maximum radius up to which the image is inverted
     #########################################################################
     if transpose:
         data = np.transpose(data)
     data = data / np.sum(np.sum(data))        
 
     abel_obj = Abel_object(data, center_y, center_x, d_alpha_deg, dr, N, Rmax)    
-    abel_obj.show(data)
+    # abel_obj.show(data)
     abel_obj.precalculate()
     t = 0
+    L = 1
     t_init = time.time()
-    for i in range(1):
+
+    for i in range(L):
         t_init = time.time()
         abel_obj.invert()
         t += time.time()-t_init        
-    print(t/50)
+    print(t/L)
 
     fig = plt.figure(num=None, figsize=(9, 4.5), dpi=100, tight_layout=True)
     ax = plt.subplot(1,1,1)
@@ -379,37 +361,45 @@ if __name__ == '__main__':
     plt.show()
 
 
-
+    L = 2048
     reconstr = True
     if reconstr:
+
+        S_inv = abel_obj.make_inverse_image(L,4,1)
         # Reconstruction of the Abel-inverted image
-        Nr = int(Rmax/dr)
-        Rvect = np.linspace(dr, Rmax, Nr)
-        Sinv = np.zeros([2048, 2048])
-        X_vect = np.arange(2048)-center_x
-        Y_vect = np.arange(2048)-center_y
+        # Nr = int(Rmax/dr)
+        # Rvect = np.linspace(dr, Rmax, Nr)
+        # Sinv = np.zeros([2048, 2048])
+        # X_vect = np.arange(2048)-center_x
+        # Y_vect = np.arange(2048)-center_y
+        
+        # h = {}
+        # for i in range(2*N):
+        #     h[i] = interp1d(Rvect, abel_obj.F[i])
+        # # h0 = interp1d(Rvect, abel_obj.F[0])
+        # # h2 = interp1d(Rvect, abel_obj.F[2])
+        # # h4 = interp1d(Rvect, abel_obj.F[4])
+        # # h6 = interp1d(Rvect, abel_obj.F[6])
 
-        h0 = interp1d(Rvect, abel_obj.F[0])
-        h2 = interp1d(Rvect, abel_obj.F[2])
-        h4 = interp1d(Rvect, abel_obj.F[4])
-        h6 = interp1d(Rvect, abel_obj.F[6])
-
-        L = len(X_vect)
-        S = np.zeros([L**2])
-        R = np.sqrt(X_vect**2+Y_vect[:,np.newaxis]**2).flatten()
-        cosTh = np.cos(np.arctan2(X_vect,Y_vect[:,np.newaxis])).flatten()
-        mask = np.logical_and(R>0, R<Rmax)
-        cosTh = cosTh[mask]
-        R = R[mask]
-        S[mask] = 1/R*(h0(R) 
-                        + h2(R)*eval_legendre(2, cosTh) 
-                        + h4(R)*eval_legendre(4, cosTh) 
+        # L = len(X_vect)
+        # S = np.zeros([L**2])
+        # R = np.sqrt(X_vect**2+Y_vect[:,np.newaxis]**2).flatten()
+        # cosTh = np.cos(np.arctan2(X_vect,Y_vect[:,np.newaxis])).flatten()
+        # mask = np.logical_and(R>0, R<Rmax)
+        # cosTh = cosTh[mask]
+        # R = R[mask]
+        # S[mask] = 1/R*(np.array([h[i](R)*eval_legendre(i,cosTh) for i in range(2*N)]).sum(axis=0)) 
+        # S[mask] = 1/R*(h0(R) 
+        #                 + h2(R)*eval_legendre(2, cosTh) 
+        #                 + h4(R)*eval_legendre(4, cosTh) 
                         
-                            )
+        #                     )
         # Sinv =np.transpose(S.reshape([L,L]))    
-        Sinv = S.reshape([L,L])   
-        plt.figure()
-        plt.imshow(Sinv, origin='lower', cmap='jet')
+        # Sinv = S.reshape([L,L])   
+        fig_im, axs_im = plt.subplots(2, 1,sharex= True,sharey='col')  
+        # plt.imshow(Sinv, origin='lower', cmap='jet')
+        axs_im[0].imshow(S_inv, origin='lower', cmap='jet')
+        axs_im[1].imshow(data, origin='lower', cmap='jet')
 
 
 plt.show()
